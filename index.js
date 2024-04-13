a397af06a84967b0889b61e18b1c1066a50f5c8e
Shawn1042 <shawnholmes9921@gmail.com>
2024-04-13 03:53:13 -0400

    dynamically added lazy loading to all images

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const e=document.getElementById("rainBackgroundContainer");for(let n=0;50>n;n++){const t=document.createElement("div");t.classList.add("raindrop");const a=100*Math.random(),o=1.5*Math.random(),r=20*Math.random()+20,c=.4*Math.random()+.3;t.style.left=`${a}%`,t.style.width=`${o}px`,t.style.height=`${r}px`,t.style.opacity=Math.random(),t.style.animationDuration=`${c}s`,t.style.animationDelay=`${2*Math.random()}s`,e.appendChild(t)}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgElements = document.querySelectorAll('img');
+
+    imgElements.forEach(img => {
+        img.loading = 'lazy';
+    });
+});
